5f35133dd227eef86ce351a6eded94394adfb621
Mikhail Zinchik <MVMGroup888@yandex.ru>
2024-01-11 10:01:14 +0300

    add animation

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,10 +1,26 @@
 import './AboutUs.css'
-
+import React from "react";
 import iphone from '../../images/iphone.svg';
 
 const AboutUs = () => {
+
+  // const [scrolled, setScrolled] = useState(false);
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const scrollY = window.scrollY;
+  //     setScrolled(scrollY > 180); // Измените значение, чтобы задать порог прокрутки
+  //   };
+
+  //   window.addEventListener('scroll', handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
+
   return (
-    <div className='aboutus__wrapper'>
+    <div className="aboutus__wrapper">
       <div className="aboutus__header">О Нас</div>
       <img className="aboutus__image" src={iphone}></img>
     </div>);
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './Main.css';
 import iphone from '../../images/iphone.svg';
 
 const Main = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  const animatedTexts = [" маникюр", " педикюр", " шугаринг", " стрижку"];
+  const sentence = "С помощью нас вы легко можете записаться на ";
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setScrolled(scrollY > 150); // Измените значение, чтобы задать порог прокрутки
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
+  useEffect(() => {
+    const showNextText = () => {
+      setCurrentTextIndex((currentIndex) => (currentIndex + 1) % animatedTexts.length);
+    };
+
+    const intervalId = setInterval(showNextText, 2000);
+
+    return () => clearInterval(intervalId);
+  }, [animatedTexts]);
+
   return (
-    <div className="main__wrapper">
-      <h1 className="main__header">Okoshko</h1>
-      <img className="main__image" src={iphone}></img>
-    </div>);
+    <div className={`main__wrapper ${scrolled ? 'scrolled' : ''}`}>
+      <div className="text-container">
+        {animatedTexts.map((text, index) => (
+          <span key={index} className={`animated-text ${currentTextIndex === index ? 'visible' : ''}`}>
+            {sentence} {text}
+          </span>
+        ))}
+      </div>
+      <img className={`main__image ${scrolled ? 'scrolled' : ''}`} src={iphone} alt="iPhone" />
+    </div>
+  );
 }
 
 export default Main;
